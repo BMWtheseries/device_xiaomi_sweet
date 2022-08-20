85dfed46d0a75ebe17b5f7d574343db1bfb49484
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-08-20 08:52:17 +0700

    sweet: Ancient-IFY
    Change-Id: I02af6fecd58eee1a40c26684bab33c4eb104a5dc
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+rm -rf hardware/qcom/sm8150/display
+rm -rf hardware/qcom/sm8150/media
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/ancient/compat
+rm -rf hardware/ancient/livedisplay
+rm -rf packages/apps/Aperture
+echo "delete hals"
+git clone https://github.com/LineageOS/android_hardware_qcom_sm8150_display -b lineage-20.0 hardware/qcom/sm8150/display
+git clone https://github.com/LineageOS/android_hardware_qcom_sm8150_media -b lineage-20.0 hardware/qcom/sm8150/media
+git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-20.0 hardware/ancient/compat
+git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay -b lineage-20.0 hardware/ancient/livedisplay
+git clone https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
+echo "cloned hals"
