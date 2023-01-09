78eb1a107dc372bf519dfadde3749fc28c605304
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-01-10 00:05:34 +0700

    sweet: Nusantara Project
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- nad_sweet.mk
+++ nad_sweet.mk
@@ -6,11 +6,19 @@
 
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Official flag
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := nad_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+rm -rf hardware/nad/compat
+rm -rf hardware/nad/livedisplay
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf packages/apps/Aperture
+echo "delete hals"
+git clone https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-20.0 hardware/nad/compat
+git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay -b lineage-20.0 hardware/nad/livedisplay
+git clone https://github.com/BMWtheseries/android_hardware_qcom_audio -b 12.1-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/crdroidandroid/android_hardware_qcom_media -b 12.1-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
