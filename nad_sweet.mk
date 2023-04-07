906a46a7179cafff572b4bfa3ce49edada55603f
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-04-08 00:44:50 +0700

    sweet: Nusantara Project
    Change-Id: Ic99c765ed4a288d2de46d4e6f2faec8768c05401

--- nad_sweet.mk
+++ nad_sweet.mk
@@ -11,10 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Official flag
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+USE_PIXEL_CHARGING := true
+TARGET_BUILD_WITH_LTO := true
+
+PRODUCT_NAME := nad_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
