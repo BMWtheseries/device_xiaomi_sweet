0b6d32277d37b62881e8d3c4c1d35c6e15aac77b
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-05-23 01:43:54 +0700

    sweet: AncientOS
    Change-Id: Id32e1e548b069dd6630ce71e5e5db1da00bbb5f3

--- ancient_sweet.mk
+++ ancient_sweet.mk
@@ -11,10 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AncientOS stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+#Official Flags
+ANCIENT_OFFICIAL := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED =: true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := ancient_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
