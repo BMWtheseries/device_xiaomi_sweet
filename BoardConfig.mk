0698b649993dc0d5014edc965be011d0da321e3f
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-06-03 08:08:40 +0700

    sweet: Remove external power HAL
    We'll be importing power-libperfmgr, so this isn't needed anymore.
    
    Change-Id: Ibce6593cf3860deec1980fa473aed1517299e0ac
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,9 +159,6 @@ BOARD_USES_METADATA_PARTITION := true
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
