6551b290ba717b527427e9360e0f55b2f65acf2a
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-06-17 13:21:10 +0700

    sweet: switch vantom kernel
    Change-Id: I8ebb6e7d80b5497e1a0da29082c8247b3e80191d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
-TARGET_KERNEL_CONFIG := vendor/sweet_user_defconfig
+TARGET_KERNEL_CONFIG := sweet_defconfig
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
